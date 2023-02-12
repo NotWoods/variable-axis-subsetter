fd6cd10838446571dd18038e2566bd83bec4168c
Tiger Oakes <contact@tigeroakes.com>
2023-02-12 15:24:20 -0800

    Update axis sliders

--- src/lib/components/AxisSlider.svelte
+++ src/lib/components/AxisSlider.svelte
@@ -2,9 +2,25 @@
 	import type { AxisProxy } from '../font-worker';
 
 	const KNOWN_SHORTHANDS: Partial<Record<string, string>> = {
+		ital: 'Italic',
 		wght: 'Weight',
 		wdth: 'Width',
-		opsz: 'Optical Size'
+		opsz: 'Optical Size',
+		slnt: 'Slant',
+		CASL: 'Casual',
+		CRSV: 'Cursive',
+		FILL: 'Fill',
+		GRAD: 'Grade',
+		MONO: 'Monospace',
+		WONK: 'Wonky',
+		XOPQ: 'Thick Stroke',
+		XTRA: 'Counter Width',
+		YOPQ: 'Thin Stroke',
+		YTAS: 'Ascender Height',
+		YTDE: 'Descender Depth',
+		YTFI: 'Figure Height',
+		YTLC: 'Lowercase Height',
+		YTUC: 'Uppercase Height'
 	};
 
 	export let axis: AxisProxy;
@@ -12,11 +28,13 @@
 
 	$: id = `${axis.axisTag}-slider`;
 	$: displayValue = value ?? axis.defaultValue;
+	$: fullName = KNOWN_SHORTHANDS[axis.axisTag];
 </script>
 
 <div class="axis-slider">
 	<label for={id}>
-		<abbr title={KNOWN_SHORTHANDS[axis.axisTag]}>{axis.axisTag}</abbr>
+		<abbr class:has-full-name={fullName}>{axis.axisTag}</abbr>
+		{#if fullName}<span class="full-name">{fullName}</span>{/if}
 	</label>
 	<output for={id} aria-live="off">{displayValue}</output>
 	<input
@@ -53,12 +71,21 @@
 	.axis-slider:last-child {
 		border-bottom-width: 0;
 	}
+
 	label {
 		grid-area: tag;
 	}
 	abbr {
 		text-decoration: none;
 	}
+	abbr.has-full-name {
+		opacity: 0.75;
+		font-size: 0.8rem;
+	}
+	.full-name {
+		display: block;
+	}
+
 	output {
 		grid-area: output;
 	}
