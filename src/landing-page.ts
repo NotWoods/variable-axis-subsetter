00cbe9f185ecc0981f01f0c59672cc2e8682c9c7
Tiger Oakes <contact@tigeroakes.com>
2023-02-11 17:30:02 -0800

    Add back/forward nav

--- src/landing-page.ts
+++ src/landing-page.ts
@@ -4,21 +4,48 @@ import './app.css';
 
 const lazyApp = import('./App.svelte');
 
+const appContainer = document.getElementById('app')!;
+
+let unloadedLandingPage: DocumentFragment | undefined;
+let loadedApp: InstanceType<Awaited<typeof lazyApp>['default']> | undefined;
+
 async function openEditor(file: ArrayBuffer) {
 	const [{ default: App }] = await Promise.all([lazyApp, setFontFace(file)]);
-	history.pushState(null, '', '/editor');
+	history.pushState(file, '', '/editor');
 
-	const target = document.getElementById('app')!;
-	while (target.firstChild) {
-		target.removeChild(target.firstChild);
+	unloadedLandingPage = document.createDocumentFragment();
+	while (appContainer.firstChild) {
+		const child = appContainer.firstChild;
+		appContainer.removeChild(child);
+		unloadedLandingPage.appendChild(child);
 	}
-	new App({
-		target,
+	loadedApp = new App({
+		target: appContainer,
 		props: {
 			fontWorker: new FontWorker(file)
 		}
 	});
 }
+window.addEventListener('popstate', (event) => {
+	switch (document.location.pathname) {
+		case '/':
+			loadedApp?.$destroy();
+			if (unloadedLandingPage) {
+				appContainer.append(unloadedLandingPage);
+				unloadedLandingPage = undefined;
+			} else {
+				location.href = '/';
+			}
+			break;
+		case '/editor':
+			if (event.state) {
+				openEditor(event.state);
+			} else {
+				location.href = '/';
+			}
+			break;
+	}
+});
 
 document.querySelector('input[type="file"]')!.addEventListener('change', async (event) => {
 	const input = event.currentTarget as HTMLInputElement;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
+// @ts-check
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+/** @return {import('vite').Plugin} */
+function redirectPlugin() {
+	return {
+		name: 'redirect',
+		configureServer(server) {
+			server.middlewares.use('/editor', (_req, res) => {
+				res.writeHead(307, { Location: '/' });
+				res.end();
+			});
+		}
+	};
+}
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [svelte()],
+	plugins: [svelte(), redirectPlugin()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
